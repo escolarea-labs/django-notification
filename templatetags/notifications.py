0edab8c3d9c31bd709469b3c8d6ac25c7de7e38e
lfborjas <luis.borjas@escolarea.com>
2010-08-25 04:17:52 -0600

    using content_types to filter context-based unread messages

--- templatetags/notifications.py
+++ templatetags/notifications.py
@@ -3,5 +3,11 @@ register = template.Library()
 from notification.models import Notice
 
 @register.filter
-def unread_notifications(user, context):
-    return Notice.objects.unseen_count_for(user, context=context)
+def unread_notifications(user, context_path, context_id):
+    if context_path and context_id is not None:     
+        splt = context_path.split('.')   
+        return Notice.objects.unseen_count_for(user, context__content_type__app_label=splt[0],
+                                                context__content_type__model=splt[1])
+        
+    else:
+        return Notice.objects.unseen_count_for(user)
