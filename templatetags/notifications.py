0eaaf1a0be15f31f6e6b57cf1e53c1703e5847a2
lfborjas <luis.borjas@escolarea.com>
2010-08-25 04:25:06 -0600

    kwargs issue querying

--- templatetags/notifications.py
+++ templatetags/notifications.py
@@ -6,6 +6,7 @@ from notification.models import Notice
 def unread_notifications(user, context):
     from django.contrib.contenttypes.models import ContentType
     if context:
-        return Notice.objects.unseen_count_for(user, content_type=ContentType.objects.get_for_model(context), object_id=context.pk)
+        return Notice.objects.unseen_count_for(user, context__content_type=ContentType.objects.get_for_model(context),
+                                                context__object_id=context.pk)
     else:
         return Notice.objects.unseen_count_for(user)
