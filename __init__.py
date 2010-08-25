bcdb12a58bcc591a97b876157f79ba66c82abe90
lfborjas <luis.borjas@escolarea.com>
2010-08-25 03:09:01 -0600

    changed everything so it is suitable for a submodule; if you want a pluggable app, use the master branch. The LICENSE an AUTHOR files now are in the __init__.py module

---
+++ __init__.py
@@ -0,0 +1,49 @@
+"""
+Copyright (c) 2008 James Tauber and contributors
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+
+The PRIMARY AUTHORS are:
+
+	* James Tauber
+	* Brian Rosner
+	* Jannis Leidel
+
+ADDITIONAL CONTRIBUTORS include:
+
+	* Eduardo Padoan
+	* Fabian Neumann
+	* Juanjo Conti
+	* Michael Trier
+"""
+
+
+VERSION = (0, 1, 4, "final")
+
+def get_version():
+    if VERSION[3] != "final":
+        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
+
+__version__ = get_version()
