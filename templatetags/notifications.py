f117d339a721c750c5909314c15670c0f1443912
lfborjas <luis.borjas@escolarea.com>
2010-08-25 04:23:55 -0600

    actually using content_types for unread message template tag

--- templatetags/notifications.py
+++ templatetags/notifications.py
@@ -3,11 +3,9 @@ register = template.Library()
 from notification.models import Notice
 
 @register.filter
-def unread_notifications(user, context_path, context_id):
-    if context_path and context_id is not None:     
-        splt = context_path.split('.')   
-        return Notice.objects.unseen_count_for(user, context__content_type__app_label=splt[0],
-                                                context__content_type__model=splt[1])
-        
+def unread_notifications(user, context):
+    from django.contrib.contenttypes.models import ContentType
+    if context:
+        return Notice.objects.unseen_count_for(user, content_type=ContentType.objects.get_for_model(context), object_id=context.pk)
     else:
         return Notice.objects.unseen_count_for(user)
